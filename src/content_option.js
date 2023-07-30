34492e7e9c0c9ea0cffa18add8737fff93d8d739
mohan <patidarmohan0907@gmail.com>
2023-07-30 11:21:11 +0530

    update code

--- src/content_option.js
+++ src/content_option.js
@@ -11,28 +11,23 @@ const introdata = {
         second: "I code cool websites",
         third: "I develop web apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "React Full Stack Developer with 2.5 years of Industry experience in building responsive web application like finance, logistics etc )",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: `Know Who I'M`,
+    aboutme: `Hi Everyone, I am Mohan Patidar from Madhya Pradesh, India.I am currently employed as a software developer at Dispatchtarck.`,
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "associate software developer",
+        where: "Pune",
+        date: "2021-22",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "software developer",
+        where: "remote",
+        date: "2022-present",
     },
 ];
 
